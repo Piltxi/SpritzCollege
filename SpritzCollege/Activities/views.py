6ca3ec8a420e3eed2c6f1f5523dea671009f25e0
Elia Pitzalis <eliapitzalis@hotmail.com>
2024-05-10 18:01:39 +0200

    EW: courses - views and href

--- SpritzCollege/Activities/views.py
+++ SpritzCollege/Activities/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Event, Course
 
 # Create your views here.
+
+class EventsList (ListView):
+    title = "Events"
+    model = Event
+    template_name = "Activities/events.html"
+
+class CoursesList (ListView):
+    title = "Courses"
+    model = Course
+    template_name = "Activities/courses.html"
